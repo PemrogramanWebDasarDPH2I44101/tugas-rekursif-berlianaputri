216508c036ba802c37e7a87ec6b4559279f52cd1
berlianaputri <42725251+berlianaputri@users.noreply.github.com>
2018-09-10 18:18:20 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,35 @@
 <?php
 
 function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
+  echo "*";
+  $i++;
+  if($i<6)
     rekursif($i);
 }
-rekursif(5);
+
+function rekursif1($i){
+  rekursif($i);
+  echo "<br>";
+  $i++;
+  if($i < 6)
+    rekursif1($i);
+}  
+rekursif1(1);
+echo "/*======================================================================*/<br>";
+
+function Piramida($x, $y){
+  echo $x;
+  $x++;
+  if($x <= $y)
+    Piramida($x, $y);
+}
+
+function RekPiramida($x, $y){
+  Piramida(1, $x);
+  echo "<br>";
+  $x++;
+  if($x <= $y)
+    RekPiramida($x, $y);
+}
+RekPiramida(1, 5);
 ?>
